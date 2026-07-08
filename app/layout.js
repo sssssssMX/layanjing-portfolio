4af508477cb9f0237f543b325dbb315ab7d2e368
sssssssMX <1107675480@qq.com>
2026-07-09 04:59:52 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css';
 
 export const metadata = {
-  title: '辣眼睛空间摄影作品集',
-  description: '辣眼睛视觉空间摄影作品集'
+  title: '辣眼精空间摄影作品集',
+  description: '辣眼精视觉空间摄影作品集'
 };
 
 export default function RootLayout({ children }) {
